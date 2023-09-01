87febc7f9a8c33d1ea9db7ed50cc6afbb4a3d695
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-09-01 08:38:08 +0300

    Refactoring Tab logic to not include inline event declarations in html

--- Tabs.js
+++ Tabs.js
@@ -1,25 +1,67 @@
-const initialTab = "Skills";
+initializeButtonEvents();
+const firstTabToOpen = "Skills";
+handleTabs(firstTabToOpen);
 
-// Gotta understand this shit later
-function openTab(evt, tabName) {
+function initializeButtonEvents()
+{
+    let tabButtons = document.querySelectorAll(".tablinks");
 
-    let i;
-    let tabContent;
-    let tabLinks;
-  
-    tabContent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
+    for (let i = 0; i < tabButtons.length; i++) {
+
+        let btn = tabButtons[i];
+
+        console.log(btn);
+        console.log(btn.innerHTML);
+
+        btn.addEventListener("click", function(){
+            handleTabs(btn.innerHTML);
+        })
     }
-  
-    tabLinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tabLinks.length; i++) {
-      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-    }
-  
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-
 }
 
-document.getElementById(initialTab).style.display = "block";
+function handleTabs(tabName)
+{
+    let tabContent;
+
+    tabContent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabContent.length; i++) {
+        
+        let content = tabContent[i];
+        
+        if (content.id === tabName)
+        {
+            openTab(content);
+        }
+        else
+        {
+            closeTab(content);
+        }
+    }
+}
+
+function openTab(tab)
+{
+    let buttons;
+    buttons = document.getElementsByClassName("tablinks");
+    
+    tab.style.display = "block";
+ 
+    for (let i = 0; i < buttons.length; i++) {
+
+        let btn = buttons[i];
+        
+        if (btn.innerHTML === tab.id)
+        {
+            btn.className += " active";
+        }
+        else
+        {
+            btn.className = btn.className.replace(" active", "");
+        }
+    }
+}
+
+function closeTab(tab)
+{
+    tab.style.display = "none";
+}
